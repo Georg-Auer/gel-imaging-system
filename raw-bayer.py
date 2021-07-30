c6c0358a6a4595d088f88912e97ba7af95c3e5b5
G-code-SPOC <georg.auer@gmail.com>
2021-07-30 10:54:18 +0200

    no normalization for rgb

--- raw-bayer.py
+++ raw-bayer.py
@@ -216,10 +216,12 @@ axarr[0, 1].imshow(green1, cmap='gray', vmin=0, vmax=bitrate, interpolation='non
 axarr[0, 1].set_title('green1')
 # axarr[1, 0].imshow(green2, cmap='gray', vmin=0, vmax=bitrate, interpolation='none')
 # axarr[1, 0].set_title('green2')
-axarr[1, 0].imshow(rgb, vmin=0, vmax=255, interpolation='none')
+axarr[1, 0].imshow(rgb, vmin=0, vmax=255, norm=None, interpolation='none')
 axarr[1, 0].set_title('rgb')
 axarr[1, 1].imshow(red, cmap='gray', vmin=0, vmax=bitrate, interpolation='none')
 axarr[1, 1].set_title('red')
+
+plt.show()
 
 # axarr[0, 0].imshow(green1, cmap='gray', vmin=0, vmax=1023)
 # axarr[0, 0].set_title('green1')
@@ -229,9 +231,6 @@ axarr[1, 1].set_title('red')
 # axarr[1, 0].set_title('green1')
 # axarr[1, 1].imshow(green1, cmap='gray', vmin=0, vmax=1023)
 # axarr[1, 1].set_title('green1')
-
-plt.show()
-
 
 # At this point we now have the raw Bayer data with the correct values
 # and colors but the data still requires de-mosaicing and
@@ -254,70 +253,3 @@ plt.show()
 # output = (output >> 2).astype(np.uint8)
 # with open('image.data', 'wb') as f:
 #     output.tofile(f)
-
-
-# import picamera
-# from io import BytesIO
-# from pydng.core import RPICAM2DNG
-
-
-
-# with picamera.PiCamera() as camera:
-
-#     stream = BytesIO()
-#     camera.capture(stream, 'jpeg', bayer=True)
-
-#     d = RPICAM2DNG()
-#     output = d.convert(stream)
-
-#     with open('image.dng', 'wb') as f:
-#             f.write(output)
-
-# from picamraw import PiRawBayer, PiCameraVersion
-
-# raw_bayer = PiRawBayer(
-#     filepath='testraw.jpg',  # A JPEG+RAW file, e.g. an image captured using raspistill with the "--raw" flag
-#     camera_version=PiCameraVersion.V2,
-#     sensor_mode=0
-# )
-# raw_bayer.bayer_array   # A 16-bit 2D numpy array of the bayer data
-# raw_bayer.bayer_order   # A `BayerOrder` enum that describes the arrangement of the R,G,G,B pixels in the bayer_array
-# raw_bayer.to_rgb()      # A 16-bit 3D numpy array of bayer data collapsed into RGB channels (see docstring for details).
-# raw_bayer.to_3d()       # A 16-bit 3D numpy array of bayer data split into RGB channels (see docstring for details).
-
-# from pidng.core import RAW2DNG, DNGTags, Tag
-# import numpy as np
-# import struct
-
-# # image specs
-# width = 4096
-# height = 3072
-# bpp= 12
-
-# # load raw data into 16-bit numpy array.
-# numPixels = width*height
-# rawFile = 'extras/scene_daylight_211ms_c2.raw16'
-# rf = open(rawFile, mode='rb')
-# rawData = struct.unpack("H"*numPixels,rf.read(2*numPixels))
-# rawFlatImage = np.zeros(numPixels, dtype=np.uint16)
-# rawFlatImage[:] = rawData[:] 
-# rawImage = np.reshape(rawFlatImage,(height,width))
-# rawImage = rawImage >> (16 - bpp)
-
-
-# does not work for the IMX477 in the hq cam
-#  # https://picamera.readthedocs.io/en/release-1.13/recipes2.html#raw-bayer-data-captures
-# import time
-# import picamera
-# import picamera.array
-# import numpy as np
-
-# with picamera.PiCamera() as camera:
-#     with picamera.array.PiBayerArray(camera) as stream:
-#         camera.capture(stream, 'jpeg', bayer=True)
-#         # Demosaic data and write to output (just use stream.array if you
-#         # want to skip the demosaic step)
-#         # output = (stream.demosaic() >> 2).astype(np.uint8)
-#         output = (stream.array() >> 2).astype(np.uint8)
-#         with open('image.data', 'wb') as f:
-#             output.tofile(f)
